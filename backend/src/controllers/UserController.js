a49c673c7526bb3b9dde159b8144b56ff831cadd
RSNF <ray1001novaes@gmail.com>
2022-10-20 01:10:29 -0300

    Geração de JWT

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -2,99 +2,83 @@ const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const slugify = require("slugify");
 const jwt = require("jsonwebtoken");
-const SECRET = "123";
 
 const { PrismaClient } = require("@prisma/client");
-const { nextTick } = require("process");
 
 const prisma = new PrismaClient();
 
 module.exports = {
-  async list(_req, res) {
-    const result = await prisma.usuarios.findMany();
+    async list(_req, res) {
+        const result = await prisma.usuarios.findMany();
 
-    return res.json(...result);
-  },
-  async show(req, res) {
-    const { id } = req.params;
+        return res.json(...result);
+    },
+    async show(req, res) {
+        const { id } = req.params;
 
-    const result = await prisma.usuarios.findUnique({
-      where: { id: id },
-    });
+        const result = await prisma.usuarios.findUnique({
+            where: { id: id },
+        });
 
-    return res.json(result);
-  },
-  async signIn(req, res, next) {
-    try {
-      const token = authorization.split(" ")[1];
+        return res.json(result);
+    },
+    async signIn(req, res) {
+        const { email, senha } = req.body;
 
-      const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-      req.payload = payload;
-    } catch (err) {
-      res.status(401);
-      if (err.email === "TokenExpiredError") {
-        throw new Error(err.email);
-      }
-      throw new Error("🚫 Un-Authorized 🚫");
-    }
+        const user = await prisma.usuarios.findUnique({
+            where: {
+                email: email,
+            },
+        });
 
-    return next();
-  } /*
-  async signIn(req, res, next) {
-    const { email, senha } = req.body;
-    const user = await prisma.usuarios.findUnique({
-      where: {
-        email: email,
-      },
-    });
+        if (!user) {
+            console.error("User cannot be found!");
 
-    if (!user) {
-      const token = req.headers["x-access-token"];
-      jwt.verify(token, SECRET, (err, decoded) => {
-        if (err) return res.status(401).end();
+            res.statusMessage = "User cannot be found!";
+            return res.status(404).send();
+        } else {
+            const checkPassword = bcrypt.compareSync(senha, user.senha);
 
-        req._users = decoded._users;
-        next();
-      });
-    } else {
-      const checkPassword = bcrypt.compareSync(senha, user.senha);
+            if (!checkPassword) {
+                res.statusMessage = "Email address or password not valid!";
+                return res.status(400).send();
+            }
+        }
 
-      if (!checkPassword) {
-        console.error("Email address or password not valid");
-      }
-    }
+        let jwtSecretKey = process.env.JWT_SECRET_KEY;
 
-    delete user.senha;
+        delete user.senha;
 
-    return res.json(user);
-  },*/,
-  async create(req, res) {
-    const { nome, email, senha } = req.body;
-    const slug = slugify(nome, { lower: true });
-    const created_at = new Date().toISOString();
-    const updated_at = new Date().toISOString();
+        const token = jwt.sign({ id: user.id }, jwtSecretKey)
 
-    await prisma.usuarios
-      .create({
-        data: {
-          id: crypto.randomBytes(4).toString("HEX"),
-          slug: slug,
-          nome: nome,
-          email: email,
-          senha: bcrypt.hashSync(senha),
-          created_at: created_at,
-          updated_at: updated_at,
-        },
-      })
-      .then(async (_users, callback = () => {}) => {
-        await prisma.$disconnect();
-        callback(res.status(200).send());
-      })
-      .catch(async (e, callback = () => {}) => {
-        console.error(e);
-        await prisma.$disconnect();
-        callback(res.status(500).send());
-        process.exit(1);
-      });
-  },
+        res.cookie("auth-token", token);
+
+        return res.json(user);
+    },
+    async create(req, res) {
+        const { nome, email, senha } = req.body;
+        const slug = slugify(nome, { lower: true });
+        const created_at = new Date().toISOString();
+        const updated_at = new Date().toISOString();
+
+        await prisma.usuarios.create({
+            data: {
+                id: crypto.randomBytes(4).toString("HEX"),
+                slug: slug,
+                nome: nome,
+                email: email,
+                senha: bcrypt.hashSync(senha),
+                created_at: created_at,
+                updated_at: updated_at,
+            },
+        }).then(async (_users, callback = () => { }) => {
+            await prisma.$disconnect();
+            callback(res.status(200).send());
+        }).catch(async (e, callback = () => { }) => {
+            console.error(e);
+            await prisma.$disconnect();
+            callback(res.status(500).send());
+            process.exit(1);
+        });
+    },
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+dotenv.config();
 
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
