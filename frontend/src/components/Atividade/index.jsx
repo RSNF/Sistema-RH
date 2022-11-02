0b5aa38cc40d3633060b2e6b508b30b8bc751598
natanias <Nataniasdopb@gmail.com>
2022-11-02 17:30:23 -0300

    Dashboard Finalizado

--- frontend/src/components/Atividade/index.jsx
+++ frontend/src/components/Atividade/index.jsx
@@ -1,5 +1,6 @@
 import "./style.css";
 import { FiUser, FiClock } from "react-icons/fi";
+import { BiPieChartAlt } from "react-icons/bi";
 
 export const Atividade = ({ href, text, target }) => {
   return (
@@ -8,6 +9,60 @@ export const Atividade = ({ href, text, target }) => {
         <section className="atividades-grid item1">
           <div className="text-principal">
             <h3 className="text-font">Atividades Recentes nas Vagas</h3>
+          </div>
+          <div className="grid__container">
+            <div className="grid__container__item">
+              <h4 className="grid__text">Gerente de Projetos</h4>
+              <div className="container__item__lab">
+                <span className="container__item__icons">
+                  <BiPieChartAlt />
+                </span>
+                <span className="container__item__text">
+                  <p>Aberto há 23 dias</p>
+                </span>
+              </div>
+              <div className="container__item__lab">
+                <span className="container__item__icons2">
+                  <FiUser />
+                </span>
+                <span className="container__item__text">
+                  <p>Candidatos: 6</p>
+                </span>
+              </div>
+              <div className="container__item__button">
+                <button className="container__item__button__acess">
+                  Acessar
+                </button>
+              </div>
+            </div>
+
+            <div className="grid__container__item">
+              <h4 className="grid__text">Designer</h4>
+              <div className="container__item__lab">
+                <span className="container__item__icons">
+                  <BiPieChartAlt />
+                </span>
+                <span className="container__item__text">
+                  <p>Aberto há 2 dias</p>
+                </span>
+              </div>
+              <div className="container__item__lab">
+                <span className="container__item__icons2">
+                  <FiUser />
+                </span>
+                <span className="container__item__text">
+                  <p>Candidatos: 5</p>
+                </span>
+              </div>
+              <div className="container__item__button">
+                <button className="container__item__button__acess">
+                  Acessar
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="grid__container__footer">
+            <button className="container__button__footer">VER TODAS</button>
           </div>
         </section>
 
