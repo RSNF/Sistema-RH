16be6d1915c540a433ec109b87efc1d9ff38c4d5
natanias <Nataniasdopb@gmail.com>
2022-11-22 21:41:34 -0300

    VisualizarCandidatos

--- frontend/src/pages/Visualizar Candidatos/index.jsx
+++ frontend/src/pages/Visualizar Candidatos/index.jsx
@@ -1,59 +1,65 @@
 import "./style.css";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import { FiChevronRight } from "react-icons/fi";
-import { Input } from "../../components/Input";
 import { Slider } from "../../components/Slider";
+import { FiCheck } from "react-icons/fi";
 
-const schema = yup
-  .object({
-    titulo: yup.string().required("O título é obrigatório!"),
-    descricao: yup.string().required("Insira um descrição para a vaga."),
-  })
-  .required();
-
-export const ObjetivosVaga = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
-
+export const VisualizarCandidatos = () => {
   return (
-    <div className="objetivos-main-div">
-      <h2 id="">Designer</h2>
-
-      <div>
+    <div className="revisar-cand-main">
+      <h2>Vaga: Designer</h2>
+      <div className="revisar-cand-name">
         <p>Nome: Jane Cooper</p>
         <p>Email: Jane@email.com</p>
         <p>Tel:(73) 98113-9007</p>
       </div>
-
-      <div className="objetivos-div">
+      <span id="cand-span">Perfil comportamental</span>
+      <div className="revisar-cand-div">
         <Slider
           label1="Colaborativo"
           label2="Independente"
           onChange={(event) => {}}
+          disabled={true}
+          vagaSlider={true}
+          defaultValue={3.6}
+          defaultValueVaga={1.5}
         />
 
-        <Slider label1="Reservado" label2="Sociável" onChange={(event) => {}} />
+        <Slider
+          label1="Reservado"
+          label2="Sociável"
+          onChange={(event) => {}}
+          disabled={true}
+          vagaSlider={true}
+          defaultValue={0.5}
+          defaultValueVaga={1.0}
+        />
 
-        <Slider label1="Intenso" label2="Paciente" onChange={(event) => {}} />
+        <Slider
+          label1="Intenso"
+          label2="Paciente"
+          onChange={(event) => {}}
+          disabled={true}
+          vagaSlider={true}
+          defaultValue={3.9}
+          defaultValueVaga={2.5}
+        />
 
         <Slider
           label1="Impulsivo"
           label2="Vigilante"
           onChange={(event) => {}}
+          disabled={true}
+          vagaSlider={true}
+          defaultValue={2.6}
+          defaultValueVaga={2.5}
         />
       </div>
 
       <div id="div-btn">
-        <button onClick={handleSubmit()}>Cancelar</button>
+        <button onClick={""}>Cancelar</button>
 
         <button type="submit" id="create-button">
           {" "}
-          <FiChevronRight /> Editar
+          <FiCheck /> Editar
         </button>
       </div>
     </div>
